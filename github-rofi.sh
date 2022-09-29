430c995b44b33496d55039b83a0d997bf18e58d4
Linus <linus@wallgren.eu>
2022-09-29 23:57:03 +0200

    Extract common code into github-rofi

---
+++ github-rofi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+pull_cache="$HOME/.cache/github-rofi/github-pulls.json"
+
+cd "$(dirname "$(readlink "${BASH_SOURCE[0]}")")" || exit 1
+./update_cache.sh
+
+rows() {
+    jq --from-file pulls_to_branches.jq --raw-output < "$pull_cache" | \
+    column -t --separator $'\t'
+}
+
+hit=$(
+    (
+        rows && \
+        echo "refresh"
+    ) | rofi -width 70 -dmenu -theme Arc-Dark -i)
+if [ -z "$hit" ]; then
+    exit 0
+fi
+if [ "$hit" == "refresh" ]; then
+    ./update_cache.sh --force
+    exit 0
+fi
+
+[[ $hit =~ ([^/]+)/([^[:space:]]+)[[:space:]]+([0-9]+)[[:space:]]+([^[:space:]]+) ]]
+
+export owner="${BASH_REMATCH[1]}"
+export repo="${BASH_REMATCH[2]}"
+export pr="${BASH_REMATCH[3]}"
+export branch="${BASH_REMATCH[4]}"
