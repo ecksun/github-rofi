80a5fa0576781ed072942a42af56ba489ec733b0
Linus <linus@wallgren.eu>
2025-08-20 09:52:56 +0200

    gitlab: Add the ability to add a custom query

--- gitlab-fetch.sh
+++ gitlab-fetch.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+config_dir="$HOME/.config/gitforge-rofi"
+
+custom='[]'
+if [ -f "$config_dir/gitlab-custom-query" ]; then
+    custom=$(curl -s \
+        --header "Authorization: Bearer $(< ~/.config/gitforge-rofi/token)" \
+        "$(<"$config_dir/gitlab-custom-query")"
+    )
+fi
+
 created=$(curl -s \
     --header "Authorization: Bearer $(< ~/.config/gitforge-rofi/token)" \
     'https://gitlab.com/api/v4/merge_requests?state=opened&scope=created_by_me'
@@ -10,4 +20,4 @@ assigned=$(curl -s \
     'https://gitlab.com/api/v4/merge_requests?state=opened&scope=assigned_to_me'
 )
 
-jq --slurp '(.[0] + .[1]) | map({ title: .title, url:  .web_url, created: .created_at, reference: .references.full, branch: .source_branch })' <(echo "$created") <(echo "$assigned")
+jq --slurp '(.[0] + .[1]  + .[2]) | map({ title: .title, url:  .web_url, created: .created_at, reference: .references.full, branch: .source_branch })' <(echo "$created") <(echo "$assigned") <(echo "$custom")
